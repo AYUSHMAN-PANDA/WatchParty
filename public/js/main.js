5adee7eb69c6d28beb8f6addaa35c283e94c0333
vjspranav <pranavasri@live.in>
2022-05-01 00:51:22 +0530

    Sync videos on new user join

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,33 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-const greetingText = document.getElementById('greetings')
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+const greetingText = document.getElementById("greetings");
 
 // Get username and room from URL
 let { username, room, customRoom } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-
 const socket = io();
 
 if (room === "SelectRoom") {
-  room = customRoom
+  room = customRoom;
 }
 // Join chatroom
-socket.emit('joinRoom', { username, room });
-
+socket.emit("joinRoom", { username, room });
 
 //add greetings text
 addGreetings(username);
 
 // Get room and users
-socket.on('roomUsers', ({ room, users }) => {
+socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
 // Message from server
-socket.on('message', (message) => {
+socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
 
@@ -38,7 +36,7 @@ socket.on('message', (message) => {
 });
 
 // Message submit
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get message text
@@ -51,36 +49,34 @@ chatForm.addEventListener('submit', (e) => {
   }
 
   // Emit message to server
-  socket.emit('chatMessage', msg);
+  socket.emit("chatMessage", msg);
 
   // Clear input
-  e.target.elements.msg.value = '';
+  e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  if (message.username === username)
-    div.classList.add('message');
-  else
-    div.classList.add('my-message');
+  const div = document.createElement("div");
+  if (message.username === username) div.classList.add("message");
+  else div.classList.add("my-message");
 
-  const p = document.createElement('p');
-  p.classList.add('meta');
+  const p = document.createElement("p");
+  p.classList.add("meta");
   p.innerText = message.username;
   p.innerHTML += `<span>${message.time}</span>`;
   div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
+  const para = document.createElement("p");
+  para.classList.add("text");
   para.innerText = message.text;
   div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
+  document.querySelector(".chat-messages").appendChild(div);
 }
 
 //add greeting text to DOM
 function addGreetings(username) {
-  greetingText.innerText = `Welcome to the Party, ${username}`
+  greetingText.innerText = `Welcome to the Party, ${username}`;
 }
 // Add room name to DOM
 function outputRoomName(room) {
@@ -89,19 +85,92 @@ function outputRoomName(room) {
 
 // Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
+  userList.innerHTML = "";
   users.forEach((user) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerText = user.username;
     userList.appendChild(li);
   });
 }
 
 //Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+document.getElementById("leave-btn").addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
   if (leaveRoom) {
-    window.location = '../index.html';
+    window.location = "../index.html";
   } else {
   }
 });
+
+// Youtube player
+// For video
+var player;
+let curState = "";
+let curTime = 0;
+function onYouTubeIframeAPIReady() {
+  const videoId = "dQw4w9WgXcQ";
+  player = new YT.Player("player", {
+    height: "720",
+    width: "640",
+    videoId: videoId,
+    playerVars: {
+      playsinline: 1,
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+function onPlayerReady(event) {
+  let search = document.getElementById("youtube-link");
+  search.value = player.getVideoUrl();
+  // player.mute();
+  player.playVideo();
+  // Send event to get video playing on other clients
+  socket.emit("request-video-server");
+  console.log("Sent request to find playing videos");
+}
+
+function onPlayerStateChange(event) {}
+
+function changeVideo() {
+  let search = document.getElementById("youtube-link");
+  player.loadVideoById(search.value.split("watch?v=")[1]);
+}
+
+// TODO: Emit event
+socket.on("request-video-client", () => {
+  console.log("Received request to for video details");
+  let curId = player.getVideoData()["video_id"];
+  // Get current player state
+  curTime = player.getCurrentTime();
+  curState = player.getPlayerState();
+  socket.emit("video-state-first-server", { curId, curTime, curState });
+  console.log("Sent video state to server");
+});
+
+socket.on(
+  "video-state-first-client",
+  async ({ videoId, curTime, isPlaying }) => {
+    console.log("ID: ", videoId);
+    if (!videoId) {
+      console.log("No change, because I am the first client");
+      return;
+    } else {
+      console.log("I am not the first client");
+      player.loadVideoById(videoId);
+      // Sleep for 2 seconds
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      if (isPlaying == 1) {
+        player.playVideo();
+      } else {
+        player.pauseVideo();
+      }
+      player.seekTo(curTime + 2);
+    }
+  }
+);
+
+// Handle all socket events
--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 const {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
-} = require('./utils/users');
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'Notifications';
+const botName = "Notifications";
+
+let roomFirstUsers = {};
 
 // Run when client connects
-io.on('connection', socket => {
-  socket.on('joinRoom', ({ username, room }) => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
 
     socket.join(user.room);
+
+    // Create an empty array for the room if it doesn't exist
+    if (!roomFirstUsers[user.room]) {
+      roomFirstUsers[user.room] = [];
+    }
 
     // Welcome current user
     // socket.emit('message', formatMessage(botName, 'Welcome to WatchParty!'));
@@ -33,39 +40,84 @@ io.on('connection', socket => {
     socket.broadcast
       .to(user.room)
       .emit(
-        'message',
+        "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       );
 
     // Send users and room info
-    io.to(user.room).emit('roomUsers', {
+    io.to(user.room).emit("roomUsers", {
       room: user.room,
-      users: getRoomUsers(user.room)
+      users: getRoomUsers(user.room),
     });
   });
 
   // Listen for chatMessage
-  socket.on('chatMessage', msg => {
+  socket.on("chatMessage", (msg) => {
     const user = getCurrentUser(socket.id);
 
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
 
   // Runs when client disconnects
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     const user = userLeave(socket.id);
 
     if (user) {
       io.to(user.room).emit(
-        'message',
+        "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
 
       // Send users and room info
-      io.to(user.room).emit('roomUsers', {
+      io.to(user.room).emit("roomUsers", {
         room: user.room,
-        users: getRoomUsers(user.room)
+        users: getRoomUsers(user.room),
       });
+    }
+  });
+
+  // Send video details
+  socket.on("video-state-first-server", ({ curId, curTime, curState }) => {
+    // Pop the user from the array
+    let room = getCurrentUser(socket.id).room;
+    const user = roomFirstUsers[room].pop();
+    console.log(
+      "sending video details to ",
+      user.username,
+      " with id ",
+      user.id
+    );
+    io.to(user.id).emit("video-state-first-client", {
+      videoId: curId,
+      curTime: curTime,
+      isPlaying: curState,
+    });
+    console.log("Sent video state to client ", user.username);
+  });
+
+  // First join video request
+  socket.on("request-video-server", () => {
+    const user = getCurrentUser(socket.id);
+    const room = user.room;
+    console.log("User ", user.username, " requested video");
+    const users = getRoomUsers(room);
+    // If user is the first user in the room
+    if (users.length === 1) {
+      console.log("== First user in room");
+      io.to(user.id).emit("video-state-first-client", {
+        videoId: false,
+        curTime: 0,
+        isPlaying: true,
+      });
+    } else {
+      console.log("== Not first user in room");
+      const firstUser = users[0];
+      console.log("First user is ", firstUser.username, " id: ", firstUser.id);
+      io.to(firstUser.id).emit("request-video-client");
+      console.log("== Sent request to ", firstUser.username);
+      // Add user to firstUsers object
+      roomFirstUsers[room].push(user);
+      console.log("== Added user to firstUsers object");
     }
   });
 });
